0e76289778477d47fc78b1ebb2ce8b2f24927250
Selahattin Sönmez <selahattin.sonmez01@hotmail.com>
2023-11-18 23:01:13 +0300

    version fixes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
 import './i18n.ts';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = document.getElementById("root");
+    render(<App />, root);
